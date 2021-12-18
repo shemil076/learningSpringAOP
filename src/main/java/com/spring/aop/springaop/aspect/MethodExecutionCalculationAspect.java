05c8ac8b4c8a88cacb68b4d7c9a822b81c665e34
Pramuditha Karunarathna <shemil076@gmail.com>
2021-12-18 23:44:45 +0530

    Using @Around advice tp implement performance tracing

---
+++ src/main/java/com/spring/aop/springaop/aspect/MethodExecutionCalculationAspect.java
@@ -0,0 +1,29 @@
+package com.spring.aop.springaop.aspect;
+
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class MethodExecutionCalculationAspect {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Around("execution(* com.spring.aop.springaop..*.*(..))")
+    public void around(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        joinPoint.proceed();
+        long timeTaken = System.currentTimeMillis() - startTime;
+
+
+
+        logger.info(" Time Taken by {} is {}", joinPoint,timeTaken);
+    }
+}
